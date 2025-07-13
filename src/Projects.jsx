5d25e1d96eab82841171d871997ca2e5dfec0e42
Glyzel Galagar <133832079+galazel@users.noreply.github.com>
2025-07-13 21:49:43 +0800

    change directory

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,31 +1,30 @@
 import Project from "./Project";
 import projects_file from "./json/projects.json";
 
-function Projects() 
-{
+function Projects() {
   return (
     <section className="projects" id="projects">
       <div className="projects-container">
-            <div className="projects-item-title">
-                <h1 className="mt-5 display-4" >Projects</h1>
-                <p>These are the projects that I created using the skills I possesed in programming.</p>
-            </div>
-            <div className="projects-item project-grid">
-            {
-                projects_file.map((item, index) => 
-                (
-                    <Project
-                       
-                        key={index}
-                        image={item.image}
-                        title={item.title}
-                        description={item.description}
-                        technology={item.technology}
-                        video={item.video}
-                        id={index}
-                    />
-                ))}
-            </div>
+        <div className="projects-item-title">
+          <h1 className="mt-5 display-4">Projects</h1>
+          <p>
+            These are the projects that I created using the skills I possesed in
+            programming.
+          </p>
+        </div>
+        <div className="projects-item project-grid">
+          {projects_file.map((item, index) => (
+            <Project
+              key={index}
+              image={item.image}
+              title={item.title}
+              description={item.description}
+              technology={item.technology}
+              video={item.video}
+              id={index}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
